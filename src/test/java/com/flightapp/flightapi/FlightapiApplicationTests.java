d03b66be257c95d7dc3462e75e0e5677906cff0f
Emre Akyüz <aemreakyuz@gmail.com>
2024-01-05 16:36:35 +0300

    first commit

---
+++ src/test/java/com/flightapp/flightapi/FlightapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightapp.flightapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
